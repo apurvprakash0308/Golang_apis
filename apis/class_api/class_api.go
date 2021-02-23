2538ce1a39433aa799a6862d83ce523018c8e082
Apurv Prakash <apurv.rathore.prakash@gmail.com>
2021-02-23 21:09:38 +0530

    adding file containing apis

---
+++ apis/class_api/class_api.go
@@ -0,0 +1,36 @@
+package class_api
+
+import (
+	"encoding/json"
+	"go-workspace/src/config"
+	"go-workspace/src/models"
+	"net/http"
+)
+
+func FindAll(response http.ResponseWriter, request *http.Request) {
+	db, err := config.GetDB()
+	if err != nil {
+		respondWithError(response, http.StatusBadRequest, err.Error())
+	} else {
+		classModel := models.ClassModel{
+			Db: db,
+		}
+		classes, err2 := classModel.FindAll()
+		if err2 != nil {
+			respondWithError(response, http.StatusBadRequest, err2.Error())
+		} else {
+			respondWithJSON(response, http.StatusOK, classes)
+		}
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
